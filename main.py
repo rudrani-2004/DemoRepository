0bc1881832a2c7fc6f338ccb6a1c6bd0dbf80358
rudrani <rudraniujankar@gmail.com>
2026-04-22 11:58:57 +0530

    addded table function to main.py file

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 ##Write a python function which will take user input as a number and 
 ### Will print the table of that number.
 
-def table(num):
+def table(no):
     for i in range(1,11):
-        print(f"{num}*{i}={num*i}")
-no=int(input("Enter the number: "))
-table(no)
+        print(f"{no}*{i}={no*i}")
+num=int(input("Enter the number: "))
+table(num)
